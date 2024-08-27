f9ebe82f6201539e364d25495056ad868bc4218d
Mike <paggaomichaeljohn@gmail.com>
2024-08-26 20:57:33 -0700

    working code

--- w3/main.js
+++ w3/main.js
@@ -20,9 +20,30 @@ function determineHouseholdPts(numberInHousehold) {
   
   let carbonFootprintPoints = 0;
   //const numberInHousehold = 3;
+
+  determineHouseholdPts(3);
   
-  
+
   // global scope
-  
-  determineHouseholdPts(3)
-  determineHouseholdPts(4)
+
+
+  function hSPts(hSize) {
+    console.log("calculating");
+    if (hSize === "large") {
+      homeSizePts = 10;
+    } else if (hSize === "medium") {
+      homeSizePts = 7;
+    } else if (hSize === "small") {
+      homeSizePts = 4;
+    } else if (hSize === "apartment") {
+      homeSizePts = 2;
+    }
+    console.log(`Because the home is ${hSize}-sized, the points would be ${homeSizePts}.`);
+  }
+
+  let homeSizePts = 0;
+
+  hSPts("large");
+  hSPts("medium");
+  hSPts("small");
+  hSPts("apartment");
