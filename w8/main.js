5cf302edafd5f4179decb11cde66ffc7a74730e8
Mike <paggaomichaeljohn@gmail.com>
2024-10-03 20:20:10 -0700

    starting code ready

---
+++ w8/main.js
@@ -0,0 +1,34 @@
+import { renderTbl } from "./render.js";
+import { determineHouseSizePts, determineHouseholdPts } from "./cfpt.js";
+
+const FORM = document.getElementById("form");
+const OUTPUT = document.getElementById("output");
+const cfpData = [];
+
+function start(first, last, houseHoldMembers, houseSize) {
+  const houseHoldPTS = determineHouseholdPts(houseHoldMembers);
+  const houseSizePTS = determineHouseSizePts(houseSize);
+  const total = houseHoldPTS + houseSizePTS;
+  cfpData.push({
+    firstName: first,
+    lastName: last,
+    houseM: houseHoldMembers,
+    houseS: houseSize,
+    houseMPTS: houseHoldPTS,
+    houseSPTS: houseSizePTS,
+    cfpTotal: total,
+  });
+}
+
+FORM.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const firstName = FORM.firstname.value;
+  const lastName = FORM.lastname.value;
+  const houseHoldMembers = parseInt(FORM.housem.value);
+  const houseSize = FORM.houses.value;
+  start(firstName, lastName, houseHoldMembers, houseSize);
+  OUTPUT.innerHTML = "";
+  //displayOutput();
+  renderTbl(cfpData);
+  FORM.reset();
+});
